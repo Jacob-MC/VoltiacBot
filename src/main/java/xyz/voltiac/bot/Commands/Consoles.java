01c4be15955d230fa63629ec465e61e37eab3049
Jacob Arvizu <65584871+Jacob-MC@users.noreply.github.com>
2021-02-22 21:48:40 -0800

    Fixed typo

--- src/main/java/xyz/voltiac/bot/Commands/Consoles.java
+++ src/main/java/xyz/voltiac/bot/Commands/Consoles.java
@@ -26,7 +26,7 @@ public class Consoles {
                                         "Just connect your phone to the same network as your console go to Join a bedrock server on the home menu and click start. In Minecraft on your console go to the friends tab and you should see a LAN game join it and you should be redirected to the server.\n" +
                                         "If you need any help please just create a ticket in <#801852792276254751>")
                                 .setColor(Color.of(51, 153, 255))
-                                .setFooter("message Executed By: " + username, avatar)).block();
+                                .setFooter("Command Executed By: " + username, avatar)).block();
                         System.out.println("Consoles message Executed By: " + username);
                     }
                 });
