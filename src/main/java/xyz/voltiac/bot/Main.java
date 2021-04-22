d66b511517cfae5be5c284ed834ab3f79375a340
Jacob-MC <jacobarvizu583@gmail.com>
2021-04-22 14:17:20 -0700

    added owner only change bot prefix command

--- src/main/java/xyz/voltiac/bot/Main.java
+++ src/main/java/xyz/voltiac/bot/Main.java
@@ -3,9 +3,14 @@ package xyz.voltiac.bot;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.presence.Activity;
 import discord4j.core.object.presence.Presence;
+import discord4j.rest.util.Color;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -43,5 +48,40 @@ public class Main {
                     client.updatePresence(Presence.online(Activity.playing("vb.help"))).block();
                 });
         client.onDisconnect().block();
+
+        // EDIT BOT PREFIX COMMAND
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .subscribe(event -> {
+                   Message message = event.getMessage();
+                   Member member = message.getAuthorAsMember().block();
+                   String messagecontent = message.getContent();
+                   String userid = member.getId().toString();
+                   String username = member.getUsername();
+                   String avatar = member.getAvatarUrl();
+                    MessageChannel channel = message.getChannel().block();
+                    if (messagecontent.equalsIgnoreCase(Main.prefix + "setbotprefix") && userid.equals("778742764908183612")) {
+                        channel.createEmbed(embedCreateSpec -> {
+                            embedCreateSpec.setTitle("**" + Main.prefix + "setbotstatus**")
+                                    .setDescription("Sets the bots status!")
+                                    .addField("**Usage:**",  Main.prefix + "setbotstatus (status)", false)
+                                    .addField("Example:",  Main.prefix + "setbotstatus Moderating Voltiac Network", false)
+                                    .setFooter("Command Executed By: " + username, avatar)
+                                    .setColor(Color.of(51, 153, 255));
+                        }).block();
+                        message.delete().block();
+                    } else if(messagecontent.toLowerCase().startsWith(Main.prefix + "setbotprefix ") && userid.equals("778742764908183612")) {
+                        int index = messagecontent.indexOf(" ");
+                        int endIndex = messagecontent.lastIndexOf(" ");
+                        String botprefix = messagecontent.substring(index, endIndex);
+                        prefix = botprefix;
+                        client.updatePresence(Presence.online(Activity.playing(botprefix))).block();
+                        channel.createMessage("Bot prefix updated.").block();
+                        System.out.println("Bot prefix set to: " + botprefix);
+
+                        message.delete().block();
+                    }
+                });
+
     }
 }
