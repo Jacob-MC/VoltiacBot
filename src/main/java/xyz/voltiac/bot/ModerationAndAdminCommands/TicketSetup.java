006639f06a69a41717efa957a5ee5db42543dcb5
Jacob-MC <jacobarvizu583@gmail.com>
2021-04-22 13:04:17 -0700

    another bug fix

--- src/main/java/xyz/voltiac/bot/ModerationAndAdminCommands/TicketSetup.java
+++ src/main/java/xyz/voltiac/bot/ModerationAndAdminCommands/TicketSetup.java
@@ -20,43 +20,46 @@ public class TicketSetup {
     public static void ticketsetup(GatewayDiscordClient client) {
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .subscribe(event -> {
-                    Message message = event.getMessage();
-                    Member member = message.getAuthorAsMember().block();
-                    String messagecontent = message.getContent();
-                    MessageChannel channel = message.getChannel().block();
-                    Message embed = null;
-                    Guild guild = event.getGuild().block();
-                    String username = member.getUsername();
-                    if (messagecontent.equalsIgnoreCase(Main.prefix + "ticketsetup") && member.getBasePermissions().block().contains(Permission.ADMINISTRATOR)) {
-                        try {
-                            message.delete().block();
-                        } catch (Exception e) {
-                        }
-                        try {
+                    try {
+                        Message message = event.getMessage();
+                        Member member = message.getAuthorAsMember().block();
+                        String messagecontent = message.getContent();
+                        MessageChannel channel = message.getChannel().block();
+                        Message embed = null;
+                        Guild guild = event.getGuild().block();
+                        String username = member.getUsername();
+                        if (messagecontent.equalsIgnoreCase(Main.prefix + "ticketsetup") && member.getBasePermissions().block().contains(Permission.ADMINISTRATOR)) {
                             try {
-                                String roles = guild.getRoles().collectList().block().toString().toLowerCase();
-                                int beginIndex = roles.indexOf("staff") - 30;
-                                int endIndex = roles.indexOf("staff");
-                                String filteredrole = roles.substring(beginIndex, endIndex);
-                                int beginIndex2 = filteredrole.indexOf("id=") + 3;
-                                int endIndex2 = filteredrole.indexOf(", name=");
-                                String roleid = filteredrole.substring(beginIndex2, endIndex2);
-
-                                embed = channel.createEmbed(embedCreateSpec -> {
-                                    embedCreateSpec.setTitle("**Ticket**")
-                                            .setDescription("React to this message to create a ticket!")
-                                            .setFooter("VoltiacBot v2.1", "https://cdn.discordapp.com/avatars/809487051564908576/32795fb85b74f559994f66fef8d9669b.png")
-                                            .setColor(Color.of(51, 153, 255));
-                                }).block();
-                                System.out.println("TicketSetup Command Executed By: " + username);
-                                embed.addReaction(ReactionEmoji.unicode("✉")).block();
-                            } catch(Exception e) {
-                                channel.createMessage("Staff role not found. Please create a role titled `staff` and assign it to the users you want to be able to access all tickets.").block().delete().timeout(Duration.ofSeconds(5)).block();
+                                message.delete().block();
+                            } catch (Exception e) {
                             }
-                        } catch (Exception e) {
-                        }
+                            try {
+                                try {
+                                    String roles = guild.getRoles().collectList().block().toString().toLowerCase();
+                                    int beginIndex = roles.indexOf("staff") - 30;
+                                    int endIndex = roles.indexOf("staff");
+                                    String filteredrole = roles.substring(beginIndex, endIndex);
+                                    int beginIndex2 = filteredrole.indexOf("id=") + 3;
+                                    int endIndex2 = filteredrole.indexOf(", name=");
+                                    String roleid = filteredrole.substring(beginIndex2, endIndex2);
+
+                                    embed = channel.createEmbed(embedCreateSpec -> {
+                                        embedCreateSpec.setTitle("**Ticket**")
+                                                .setDescription("React to this message to create a ticket!")
+                                                .setFooter("VoltiacBot v2.1", "https://cdn.discordapp.com/avatars/809487051564908576/32795fb85b74f559994f66fef8d9669b.png")
+                                                .setColor(Color.of(51, 153, 255));
+                                    }).block();
+                                    System.out.println("TicketSetup Command Executed By: " + username);
+                                    embed.addReaction(ReactionEmoji.unicode("✉")).block();
+                                } catch (Exception e) {
+                                    channel.createMessage("Staff role not found. Please create a role titled `staff` and assign it to the users you want to be able to access all tickets.").block().delete().timeout(Duration.ofSeconds(5)).block();
+                                }
+                            } catch (Exception e) {
+                            }
                         } else if (messagecontent.equalsIgnoreCase(Main.prefix + "ticketsetup") && !member.getBasePermissions().block().contains(Permission.ADMINISTRATOR)) {
                             channel.createMessage("You do not have permission to use this command!").block();
+                        }
+                    } catch (Exception e) {
                     }
                 });
     }
