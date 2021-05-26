97feceba740a8856f4a7d367d82107e4d2f4e094
Jacob-MC <jacobarvizu583@gmail.com>
2021-05-26 09:42:17 -0700

    added misc commands section, serverinfo command, and improved stability, as the last version was crashing

--- src/main/java/xyz/voltiac/bot/Main.java
+++ src/main/java/xyz/voltiac/bot/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static String prefix = "vb.";
     public static void main(String[] args) throws SQLException {
         SQLiteDataSource.getConnection();
-        GatewayDiscordClient client = DiscordClientBuilder.create(System.getenv("TOKEN"))
+        GatewayDiscordClient client = DiscordClientBuilder.create("token")
                 .build()
                 .login()
                 .block();
